1b287360c36b8f17bb24502da29d71621a4eea47
HectorZetino <h.rzc.zetino@hotmail.es>
2019-10-10 09:58:21 -0600

    Primer Commit Proyecto TSE

---
+++ ProyectoTSE_ED1/Models/votante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTSE_ED1.Models
+{
+    public class votante
+    {
+        public int DPI { get; set; }
+        public bool UNE { get; set; }
+        public bool VAMOS { get; set; }
+        public bool CREO { get; set; }
+        public bool UNIONISTA { get; set; }
+        public bool VOTONULO { get; set; }
+
+    }
+}
